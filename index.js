e31b9ad5f70545ae9d483771c1c7a6cb7b45993e
Keith Godfrey <keith.godfrey23@gmail.com>
2024-06-14 15:59:47 -0700

    added questions on index.js

--- index.js
+++ index.js
@@ -1,16 +1,95 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const genMD = require("./utils/generateMarkdown.js");
+const inquirer = require("inquirer");
+const { default: Choices } = require("inquirer/lib/objects/choices.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your project title?",
+        name: "title",
+      },
+      {
+        type: "input",
+        message: "Provide a short description of your project.",
+        name: "description",
+      },
+      {
+        type: "input",
+        message: "What are the steps required to install your project?",
+        name: "install",
+      },
+      {
+        type: "input",
+        message: "Provide instructions and examples for use.",
+        name: "instructions",
+      },
+      {
+        type: "input",
+        message: "Were there any collaborators involved in your project?",
+        name: "collaborators",
+      },
+      {
+        type: "input",
+        message: "How can others contribute to your project?",
+        name: "contribute",
+      },
+      {
+        type: "input",
+        message: "Provide testing examples for your project.",
+        name: "testing",
+      },
+      {
+        type: "list",
+        message: "What license is your project operating?",
+        name: "license",
+        Choices: [
+          "MIT License",
+          "license:apache-2.0",
+          "Creative Commons",
+          "unlicense",
+          ],
+      },
+      {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username",
+      },
+      {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is your site url?",
+        name: "url",
+      },
+    ])
+
+    .then((response) =>
+      writeToFile("README - generatedMD", genMD.generateMarkdown(response))
+    ),
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    
+  fs.writeFile(fileName, data, (err) => {
+    err ? console.error("error occured.", err) : console.log("Success!");
+  });
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions)
+  .then((answers) => {
+    createNewFile("readme, answers");
+  });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,45 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
+
+
+function renderLicenseBadge(license) {
+  switch (license) {
+    case value:
+      
+      break;
+  
+    default:
+      break;
+  }
+
+  var licBadge ="";
+  case "MIT License":
+  licBadge =
+
+
+function renderLicenseLink(license) {}
+
+
+function renderLicenseSection(license) {}
+
+
 function generateMarkdown(data) {
   return `# ${data.title}
 
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {
+  renderLicenseBadge,
+  renderLicenseLink,
+  renderLicenseSection,
+  generateMarkdown
+}
